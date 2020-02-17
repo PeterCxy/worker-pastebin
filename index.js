6062f0edd32af1ea65dabb93d9b7f4e2ff903ca7
Peter Cai <peter@typeblog.net>
2020-02-17 17:01:59 +0800

    index.js: remove useless commented code

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 import main from "./src/index.coffee"
 
 main.apply(this)
-
-/*addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})*/
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-/*
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}*/
